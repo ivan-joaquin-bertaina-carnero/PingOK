d10dbf7a443dfce1c66020b7d4a0296ae041f6cf
Iván Joaquín Bertaina Carnero <ivanpcservice@gmail.com>
2021-01-10 04:32:51 -0300

    Primera versión
    Subo el código/archivos de la primera versión.

---
+++ pingok/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.HostnameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DirecciónIPToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PingOKV10ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "PingOK v1.0"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PingOKV10ToolStripMenuItem, Me.ToolStripMenuItem2, Me.HostnameToolStripMenuItem, Me.DirecciónIPToolStripMenuItem, Me.ToolStripMenuItem1, Me.SalirToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(143, 104)
+        '
+        'HostnameToolStripMenuItem
+        '
+        Me.HostnameToolStripMenuItem.Name = "HostnameToolStripMenuItem"
+        Me.HostnameToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.HostnameToolStripMenuItem.Text = "hostname"
+        '
+        'DirecciónIPToolStripMenuItem
+        '
+        Me.DirecciónIPToolStripMenuItem.Name = "DirecciónIPToolStripMenuItem"
+        Me.DirecciónIPToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DirecciónIPToolStripMenuItem.Text = "dirección IP"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(149, 6)
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1500
+        '
+        'PingOKV10ToolStripMenuItem
+        '
+        Me.PingOKV10ToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PingOKV10ToolStripMenuItem.Name = "PingOKV10ToolStripMenuItem"
+        Me.PingOKV10ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PingOKV10ToolStripMenuItem.Text = "PingOK v1.0"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(149, 6)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Enabled = False
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HostnameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DirecciónIPToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PingOKV10ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripSeparator
+
+End Class
---
+++ pingok/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Environment
+Imports System.Net
+
+Public Class Form1
+
+    Dim estado As Integer
+    Dim hostname As String = Dns.GetHostName()
+    Dim dirIP As String = CType(Dns.GetHostByName(hostname).AddressList.GetValue(0), IPAddress).ToString
+    Function Obtener_datos_RED()
+        hostname = Dns.GetHostName()
+        dirIP = CType(Dns.GetHostByName(hostname).AddressList.GetValue(0), IPAddress).ToString
+        Return dirIP
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        Me.HostnameToolStripMenuItem.Text = hostname
+        Me.DirecciónIPToolStripMenuItem.Text = Obtener_datos_RED()
+
+        If My.Computer.Network.IsAvailable Then
+            Try
+                If My.Computer.Network.Ping("www.google.com", 1000) Then
+                    NotifyIcon1.Icon = New Icon("C:\Users\Usuario\Desktop\PruebaDeConexión\íconos\green.ico")
+                    If estado Then
+                        NotifyIcon1.BalloonTipText = "La prueba de conexión fue satisfactoria, disfrutá de Internet!"
+                        NotifyIcon1.BalloonTipTitle = "TODO FUNCIONA OK!!!"
+                        NotifyIcon1.ShowBalloonTip(10)
+                        estado = 0
+                    End If
+                Else
+                    estado = 0
+                    NotifyIcon1.Icon = New Icon("C:\Users\Usuario\Desktop\PruebaDeConexión\íconos\red.ico")
+                    If My.Computer.Network.Ping("8.8.8.8", 250) Then
+                        NotifyIcon1.BalloonTipText = "Se detectó un problema en los DNS!"
+                        NotifyIcon1.BalloonTipTitle = "ALGO ANDA MAL!!!"
+                        NotifyIcon1.ShowBalloonTip(10)
+                        estado = 1
+                    Else
+                        NotifyIcon1.BalloonTipText = "Internet parece tener microcortes / perdida de paquetes!"
+                        NotifyIcon1.BalloonTipTitle = "ALGO ANDA MAL!!!"
+                        NotifyIcon1.ShowBalloonTip(10)
+                        estado = 1
+                    End If
+                End If
+            Catch ex As Exception
+                NotifyIcon1.Icon = New Icon("C:\Users\Usuario\Desktop\PruebaDeConexión\íconos\red.ico")
+                NotifyIcon1.BalloonTipText = "Problema en el MODEM del ISP!"
+                NotifyIcon1.BalloonTipTitle = "NO HAY CONEXIÓN A INTERNET!!!"
+                NotifyIcon1.ShowBalloonTip(10)
+                estado = 1
+            End Try
+
+        Else
+            NotifyIcon1.Icon = New Icon("C:\Users\Usuario\Desktop\PruebaDeConexión\íconos\red.ico")
+            NotifyIcon1.BalloonTipText = "No hay ninguna conexion de red activa!!!"
+            NotifyIcon1.BalloonTipTitle = "PROBLEMA GRAVE!!!"
+            NotifyIcon1.ShowBalloonTip(10)
+            estado = 1
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Hide()
+        NotifyIcon1.BalloonTipText = "Comprobando conexión a Internet!!!"
+        NotifyIcon1.BalloonTipTitle = "INFORMACIÓN!!!"
+        NotifyIcon1.ShowBalloonTip(10)
+        estado = 1
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SalirToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+End Class
